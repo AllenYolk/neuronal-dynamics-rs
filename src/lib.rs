9291a2601e92edaa6a3efa82de35062051060650
AllenYolk <allen.yfhuang@gmail.com>
2023-07-18 11:36:32 +0800

    feat: add three neuron types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod hh_neuron;
+mod fn_neuron;
+mod ml_neuron;
+
 use clap::ValueEnum;
 
 #[derive(ValueEnum)]
@@ -5,4 +9,20 @@ use clap::ValueEnum;
 #[value(rename_all = "PascalCase")]
 pub enum NeuronType {
     HodgkinHuxley,
+    FitzHughNagumo,
+    MorrisLecar,
+}
+
+pub fn run(neuron_type: NeuronType) {
+    match neuron_type {
+        NeuronType::HodgkinHuxley => {
+            println!("Running Hodgkin-Huxley model...");
+        },
+        NeuronType::FitzHughNagumo => {
+            println!("Running FitzHugh-Nagumo model...");
+        },
+        NeuronType::MorrisLecar => {
+            println!("Running Morris-Lecar model...");
+        },
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use clap::Parser;
 use neuronal_dynamics_rs::NeuronType;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
+#[derive(Debug, Clone, Copy)]
 #[command(author, version, about, long_about = None)]
 #[command(next_line_help = true)]
 struct Cli {
     #[arg(value_enum)]
-    neuron: NeuronType,
+    neuron_type: NeuronType,
 }
 
 fn main() {
     let cli = Cli::parse();
     dbg!(cli);
+
+    neuronal_dynamics_rs::run(cli.neuron_type);
 }
